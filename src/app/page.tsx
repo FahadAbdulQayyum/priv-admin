e2d08ce9f06be62e82f0dd477f71afef92a499e7
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-05 01:35:40 +0500

    Commented ununsed code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import FlashSale from '@/components/FlashSale'
-import FirstLook from '@/components/FirstLook'
-import Hero from '@/components/Hero'
-import BendatApp from '@/components/BendatApp'
+// import FlashSale from '@/components/FlashSale'
+// import FirstLook from '@/components/FirstLook'
+// import Hero from '@/components/Hero'
+// import BendatApp from '@/components/BendatApp'
 import React from 'react'
-import Featured from '@/components/Featured'
-import GearUp from '@/components/Gearup'
-import DontMiss from '@/components/DontMiss'
-import Essential from '@/components/Essential'
-import CategoryList from '@/components/CategoryList'
+// import Featured from '@/components/Featured'
+// import GearUp from '@/components/Gearup'
+// import DontMiss from '@/components/DontMiss'
+// import Essential from '@/components/Essential'
+// import CategoryList from '@/components/CategoryList'
 import Admin from '@/components/Admin'
 
 
