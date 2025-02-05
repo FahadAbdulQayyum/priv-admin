aa874e6daca0ca673b264f6ad76fde848d699cfc
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-06 02:15:13 +0500

    correct sideBar page

--- src/app/sideBar/page.tsx
+++ src/app/sideBar/page.tsx
@@ -1,7 +1,8 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
-export default function SideBar({ children }: { children: React.ReactNode }) {
+// export default function SideBar({ children }: { children: React.ReactNode }) {
+export default function SideBar() {
     return (
         <SidebarProvider
         // className="pt-60"
@@ -9,7 +10,7 @@ export default function SideBar({ children }: { children: React.ReactNode }) {
             <AppSidebar />
             <main>
                 <SidebarTrigger />
-                {children}
+                {/* {children} */}
             </main>
         </SidebarProvider >
     )
