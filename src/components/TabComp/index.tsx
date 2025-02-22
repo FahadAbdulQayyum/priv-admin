d31f59f2e13a9619159ba03e57ee386975cf6664
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-23 01:41:17 +0500

    ; added

--- src/components/TabComp/index.tsx
+++ src/components/TabComp/index.tsx
@@ -37,10 +37,10 @@ export function TabComp() {
                 <Tabs defaultValue="past-jobs">
                     {/* Nested Tabs */}
                     <TabsList className="flex space-x-5 mb-4 w-full">
-                        <TabsTrigger value="past-jobs">Today&aposs Jobs (195)</TabsTrigger>
-                        <TabsTrigger value="future-jobs">Tomorrow&aposs Jobs (44)</TabsTrigger>
-                        <TabsTrigger value="yesterday-jobs">Yesterday&aposs Jobs (44)</TabsTrigger>
-                        <TabsTrigger value="cant-find-jobs">Can&apost find job?</TabsTrigger>
+                        <TabsTrigger value="past-jobs">Today&apos;s Jobs (195)</TabsTrigger>
+                        <TabsTrigger value="future-jobs">Tomorrow&apos;s Jobs (44)</TabsTrigger>
+                        <TabsTrigger value="yesterday-jobs">Yesterday&apos;s Jobs (44)</TabsTrigger>
+                        <TabsTrigger value="cant-find-jobs">Can&apos;t find job?</TabsTrigger>
                     </TabsList>
 
                     {/* Nested Tabs Content */}
