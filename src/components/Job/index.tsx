c749650ef81a7ed792513718638fb1b389d7ecb5
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-22 17:10:57 +0500

    &apos;

--- src/components/Job/index.tsx
+++ src/components/Job/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
+import { IdCard, BadgeDollarSign, FileUser, ContactRound, Clock, Fan, MapPin, TimerReset, Building2 } from 'lucide-react'
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "@/components/lib/hooks";
 import { RootState } from '@/components/lib/store';
@@ -15,7 +16,7 @@ const Job = () => {
             className="flex space-x-3 bg-white overflow-hidden overflow-x-auto scrollbar-hide p-2 items-stretch"
         >
             <div className="flex flex-col space-y-1">
-                <h1 className="shadow mb-2 font-bold text-center w-full">ID</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><IdCard className="text-sm" /><p>ID</p></h1>
                 <Button>Reassign Job</Button>
                 <Button>Agent Cancel</Button>
                 <Button>Agent Cancel w FMAP</Button>
@@ -29,7 +30,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Price</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><BadgeDollarSign className="text-sm" /><p>Price</p></h1>
                 <p>Svcs:$180</p>
                 <p>Est Tip: $30</p>
                 <p>Est PF: $24</p>
@@ -44,7 +45,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Client</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><FileUser className="text-sm" /><p>Client</p></h1>
                 <p>Ava DeSena</p>
                 <p>643 345 4456</p>
                 <p>adsena88@gmail.com</p>
@@ -57,7 +58,7 @@ const Job = () => {
             {/* <div className="flex h-full items-stretch"> */}
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Agent</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><ContactRound className="text-sm" /><p>Agent</p></h1>
                 <p>jillianocasio@me.com</p>
                 <p>female</p>
                 <Image
@@ -75,7 +76,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Job Time</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><Clock className="text-sm" /><p>Job Time</p></h1>
                 <p>February 21 2025 8:30 AM</p>
                 <label>Change Day:</label>
                 <select className="border p-1 rounded">
@@ -103,8 +104,8 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Services</h1>
-                <p>Haircut: Women's Haircut with</p>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><Fan className="text-sm" /><p>Services</p></h1>
+                <p>Haircut: Women&apos;s Haircut with</p>
                 <p>Blowout - $105 X 1</p>
                 <p>Entrance on 25th St</p>
                 <p>between L and M</p>
@@ -114,7 +115,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Address</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><MapPin className="text-sm" /><p>Address</p></h1>
                 <p>1 Grand Cypress Blvd Orlando FL 32836 undefined</p>
                 <Button>Change Addr</Button>
                 <Button>Block User</Button>
@@ -123,7 +124,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Fees</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><BadgeDollarSign className="text-sm" /><p>Fees</p></h1>
                 <label>Service Add On</label>
                 <input className="border p-1 rounded" />
                 <label>Travel Fee</label>
@@ -139,7 +140,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">Last Event</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><TimerReset className="text-sm" /><p>Last Event</p></h1>
                 <p>stillConfirmingPro:</p>
                 <p>2/22/2025, 3:17:51 PM</p>
             </div>
@@ -147,7 +148,7 @@ const Job = () => {
             <Separator orientation="vertical" />
 
             <div className="flex flex-col space-y-1 items-center">
-                <h1 className="shadow mb-2 font-bold text-center w-full">City</h1>
+                <h1 className="flex justify-center space-x-1 shadow mb-2 font-bold text-center w-full"><Building2 className="text-sm" /><p>City</p></h1>
                 <p>Washington</p>
                 <p>DC</p>
             </div>
