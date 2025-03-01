857d74da342241a1e26578ec804546ff54127527
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-01 18:28:31 +0500

    updated storeProvider.tsx file

--- src/components/lib/storeProvider.tsx
+++ src/components/lib/storeProvider.tsx
@@ -3,7 +3,6 @@ import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from '../lib/store'
 
-import { initializeCount } from '../lib/features/counter/counterSlice'
 import AppLoader from '../AppLoader'
 
 //
@@ -17,8 +16,6 @@ export default function StoreProvider({
     if (!storeRef.current) {
         // Create the store instance the first time this renders
         storeRef.current = makeStore()
-        // storeRef.current.dispatch(initializeCount({ count }))
-        storeRef.current.dispatch(initializeCount(count))
     }
 
     return <Provider store={storeRef.current}>
