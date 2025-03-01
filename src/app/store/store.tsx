931a7a4a58184445f444adb046bfa79b442a3ae9
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-01 15:24:36 +0500

    removed extra comment

--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -21,34 +21,3 @@ store.subscribe(() => {
         product: store.getState().product, // Save only the product slice
     });
 });
-
-
-
-
-
-
-
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import productReducer from './productSlice';
-// import { loadState, saveState } from './localStorage';
-
-// const persistedState = loadState();
-
-// export const store = configureStore({
-//     reducer: {
-//         product: productReducer,
-//     },
-//     preloadedState: persistedState, // Load state from localStorage
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
-// // Save the state to localStorage whenever it changes
-// store.subscribe(() => {
-//     saveState({
-//         product: store.getState().product, // Save only the product slice
-//     });
-// });
